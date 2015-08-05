ed94135a4d1e3825cc13844b70810c3e5e11b470
Sanjay Chakrapani <csanjay@gmail.com>
2015-08-05 04:33:04 -0400

    added comments to stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -12,30 +12,30 @@ var stringifyJSON = function(obj) {
   else if (typeof obj === 'string')
     return '"'+obj+'"';
   else if (typeof obj === 'object') {
-    if (obj === null)
+    if (obj === null)  // handle null objects
       return 'null';
-    else if (Array.isArray(obj)) {
+    else if (Array.isArray(obj)) {  // handle arrays
       reducedString = _.reduce(obj,function(combine,item){
         tmpString = stringifyJSON(item);
-        if (tmpString === undefined)
+        if (tmpString === undefined)  // skip functions and undefined values
           return combine;
         else
           return combine+tmpString+',';
       },'');
-      return '['+reducedString.slice(0,reducedString.length-1)+']';
+      return '['+reducedString.slice(0,reducedString.length-1)+']';  // trim end comma and add brackets
     }
-    else {
+    else {  // handle regular objects
       reducedString = _.reduce(obj,function(combine,item,key){
         tmpString = stringifyJSON(item);
-        if (tmpString === undefined)
+        if (tmpString === undefined)  // skip functions and undefined values
           return combine;
         else
           return combine+'"'+key+'"'+':'+tmpString+',';
       },'');
-      return '{'+reducedString.slice(0,reducedString.length-1)+'}';
+      return '{'+reducedString.slice(0,reducedString.length-1)+'}';  // trim end comma and add curly braces
     }
   }
-  else
+  else  // functions and undefined values
     return undefined;
 
 };
