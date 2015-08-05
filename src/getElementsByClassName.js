aa86cefc6207e930ed654ae8a7d97d2abe134341
Sanjay Chakrapani <csanjay@gmail.com>
2015-08-05 06:13:02 -0400

    getElementsByClassName works good

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,23 @@
 var getElementsByClassName = function(className
 ){
   // your code here
+  var classFilter = function(element, className) {
+
+    if (typeof element.classList !== 'undefined' && element.classList.contains(className))
+      elements.push(element);  // push element if class found
+
+    // check if element has child elements
+    if (typeof element.childNodes !== 'undefined' && element.childNodes.length > 0) {
+      _.each(element.childNodes, function(element){  // recurse child elements
+        classFilter(element, className);
+      });
+    }        
+  }
+
+  var elements = [];
+
+  classFilter (document.body, className);
+
+  return elements;
+
 };
