8dce90211ce818150ae417542ebaf6b679af3be2
Sanjay Chakrapani <csanjay@gmail.com>
2015-08-05 04:25:53 -0400

    custom stringify works now

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,37 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var tmpString, reducedString;
+
+  if (typeof obj === 'number' || typeof obj === 'boolean')
+    return String(obj);
+  else if (typeof obj === 'string')
+    return '"'+obj+'"';
+  else if (typeof obj === 'object') {
+    if (obj === null)
+      return 'null';
+    else if (Array.isArray(obj)) {
+      reducedString = _.reduce(obj,function(combine,item){
+        tmpString = stringifyJSON(item);
+        if (tmpString === undefined)
+          return combine;
+        else
+          return combine+tmpString+',';
+      },'');
+      return '['+reducedString.slice(0,reducedString.length-1)+']';
+    }
+    else {
+      reducedString = _.reduce(obj,function(combine,item,key){
+        tmpString = stringifyJSON(item);
+        if (tmpString === undefined)
+          return combine;
+        else
+          return combine+'"'+key+'"'+':'+tmpString+',';
+      },'');
+      return '{'+reducedString.slice(0,reducedString.length-1)+'}';
+    }
+  }
+  else
+    return undefined;
+
 };
